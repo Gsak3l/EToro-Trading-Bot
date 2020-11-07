a1edf9df9878a06c9e0b6d421a52bf1675afec26
gsak3l <gsakel5@gmail.com>
2020-11-07 18:47:28 +0200

    getting the rows successfully

--- main.py
+++ main.py
@@ -19,8 +19,19 @@ class Auto_trading_bot:
             if button.get_attribute('value') == 'agree':
                 button.click()  # clicking the consent button
                 break  # breaking the loop (or the if, I am not sure)
+        # clicking twice the % change on the stocks table
+        for i in range(2):
+            time.sleep(5)  # waiting for the table content to load
+            bot.find_element_by_xpath('/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[6]/div/div/section/div/'
+                                      'div[2]/div[1]/table/thead/tr/th[4]').click()
+        # getting the items of the body hopefully
+        table = bot.find_element_by_tag_name('tbody')
+        rows = table.find_elements_by_tag_name('tr')
+        print(len(rows))
 
 
 if __name__ == '__main__':
     bot1 = Auto_trading_bot()
-    bot2 = Auto_trading_bot()
+    process1 = multiprocessing.Process(target=bot1.get_tables, )
+    process1.start()
+    process1.join()
